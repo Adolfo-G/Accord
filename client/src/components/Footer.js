d0b5ab20d93a5b07534e70a82260679ec320273e
bxz5089 <69992026+bxz5089@users.noreply.github.com>
2022-05-13 16:24:20 -0400

    Updated footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -7,18 +7,13 @@ const Footer = () => {
     <footer className="w-100 mt-auto bg-secondary footer">
       <div className="text-center">
         <h4>&copy; {new Date().getFullYear()} - Accord</h4>
-        <div className="row">  
-                <h3>Contact Info:</h3>
-
-                    <div className="col d-flex justify-content-center text-center">
-                        <a href="https://github.com/Adolfo-G"><FontAwesomeIcon icon={faGithub} size='lg'/></a> 
-                        <a href="https://github.com/bxz5089"><FontAwesomeIcon icon={faGithub} size='lg'/></a> 
-                        <a href="https://github.com/mbarrientos1129"><FontAwesomeIcon icon={faGithub} size='lg'/></a> 
-                        <a href="https://github.com/Yanbud"><FontAwesomeIcon icon={faGithub} size='lg'/></a> 
-                    </div>
-
-                </div> 
-
+          <h5>Contact Info:</h5>
+              <div className="text-center">
+                  <a href="https://github.com/Adolfo-G"><FontAwesomeIcon icon={faGithub} size='lg' style={{padding:'0px 10px'}}/>Adolfo Gonzalez</a> 
+                  <a href="https://github.com/bxz5089"><FontAwesomeIcon icon={faGithub} size='lg' style={{padding:'0px 10px'}}/>Bowen Zheng</a> 
+                  <a href="https://github.com/mbarrientos1129"><FontAwesomeIcon icon={faGithub} size='lg' style={{padding:'0px 10px'}}/>Michael Barrientos</a> 
+                  <a href="https://github.com/Yanbud"><FontAwesomeIcon icon={faGithub} size='lg' style={{padding:'0px 10px'}}/>Yan Zhang</a> 
+              </div>
       </div>
     </footer>
   );
