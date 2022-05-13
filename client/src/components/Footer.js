04a3e9698b3e7f48a13b69b24961f7252ff59435
Adolfo Gonzalez <agonzal177@gmail.com>
2022-05-13 17:44:29 -0400

    added contributers in footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -6,6 +6,14 @@ const Footer = () => {
       <div className="text-center">
         <h4>&copy; {new Date().getFullYear()} - Accord</h4>
       </div>
+      <div className='footer-container'>
+      <ul className='foot-nav'>
+        <li className="nav-item"><a href="https://github.com/Adolfo-G" >Adolfo Gonzalez</a></li>
+        <li className="nav-item"><a href="https://github.com/bxz5089" >Bowen Zheng</a></li>
+        <li className="nav-item"><a href="https://github.com/mbarrientos1129" >Michael Barrientos</a></li>
+        <li className="nav-item"><a href="https://github.com/Yanbud" >Yan Zhang</a></li>
+      </ul>
+      </div>
     </footer>
   );
 };
